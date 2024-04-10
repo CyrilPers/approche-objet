dec814ad0f9971d1f09477efa283f7070e642a35
Cyril <cyril.personne@outlook.fr>
2024-04-10 17:13:20 +0200

    push

---
+++ src/main/java/fr/approche/objet/utils/TestMethodeStatic.java
@@ -0,0 +1,15 @@
+package fr.approche.objet.utils;
+
+public class TestMethodeStatic {
+    public static void main(String[] number) {
+        String chaine = "12";
+
+        int Entier = Integer.valueOf(chaine);
+        int a = 12;
+        int b = 30;
+
+        int max = Integer.max(a, b);
+
+        System.out.println(max);
+    }
+}
