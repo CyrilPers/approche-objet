3f9023c001518902af1518d62a2ed1925d35fd9b
Cyril <cyril.personne@outlook.fr>
2024-04-11 08:46:30 +0200

    push

--- src/main/java/fr/approche/objet/fr/diginamic/banque/TestOperations.java
+++ src/main/java/fr/approche/objet/fr/diginamic/banque/TestOperations.java
@@ -5,7 +5,8 @@ import java.util.Date;
 public class TestOperations {
     public static void main(String[] args) {
         Double montantOperation = 0.00;
-        Operation[] operations = {new Credit(new Date(), 100.00), new Debit(new Date(), 200.00), new Credit(new Date(), 10000.50), new Debit(new Date(), 50.00)};
+        Date date = new Date();
+        Operation[] operations = {new Credit(date, 100.00), new Debit(date, 200.00), new Credit(date, 10000.50), new Debit(date, 50.00)};
         for(Operation myOperation: operations) {
             System.out.println("Date d'opération " + myOperation.getDateOperation() + " de type " + myOperation.getType() +   " d'un montant de " + myOperation.getMontantOperation());
 
